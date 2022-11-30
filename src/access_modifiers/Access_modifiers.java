9439caa9ec27550f379927d16b6bf80885c45527
anon-bianco <anonh7@gmail.com>
2022-11-30 19:18:35 +0530

    Initial commit

---
+++ src/access_modifiers/Access_modifiers.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package access_modifiers;
+
+/**
+ *
+ * @author hilbe
+ */
+public class Access_modifiers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        student gavaskar=new student();
+        gavaskar.rollno="GAVASKAR";
+        gavaskar.display();
+    }
+    
+}
